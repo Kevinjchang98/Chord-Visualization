cb605f3027cfe33b20f1d60a32fb7a45ce973db3
Kevin Chang <kevinjchang98@gmail.com>
2022-12-01 22:38:35 -0800

    generate finger table

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -9,13 +9,14 @@ interface ChordNode {
         x: number;
         y: number;
     };
+    fingerTable: Array<{ start: number; successor: number }>;
 }
 
 export default function Canvas() {
     const [ticks, setTicks] = useState<Array<String>>([]);
     const [nodesData, setNodesData] = useState<Array<ChordNode>>([]);
     const svgRef: any = useRef();
-    const M = 5;
+    const M = 3;
     const r = 400;
     const theta = (2 * Math.PI) / Math.pow(2, M);
 
@@ -37,9 +38,47 @@ export default function Canvas() {
         let newNode: ChordNode = {
             id,
             coords,
+            fingerTable: generateFingerTable(id),
         };
 
         setNodesData([...nodesData, newNode]);
+    };
+
+    const generateFingerTable = (nodeId: number) => {
+        let fingerTable = [];
+        let nodeIds = nodesData.map((node) => node.id);
+
+        // For each row in finger table
+        for (let i = 1; i <= Math.pow(2, M) / 2; i *= 2) {
+            // j used to search for succ
+            let j = 0;
+            let start = (nodeId + i) % Math.pow(2, M);
+            let inserted = false;
+
+            // Search until we find successor
+            while (j < Math.pow(2, M)) {
+                if (nodeIds.includes((nodeId + i + j) % Math.pow(2, M))) {
+                    // If found, set this nodeId as successor to this start
+                    // value
+                    fingerTable.push({
+                        start,
+                        successor: (nodeId + i + j) % Math.pow(2, M),
+                    });
+                    inserted = true;
+                    break;
+                }
+                j++;
+            }
+
+            // Otherwise set ourselves as the successor
+            if (!inserted)
+                fingerTable.push({
+                    start: (nodeId + i) % Math.pow(2, M),
+                    successor: nodeId,
+                });
+        }
+
+        return fingerTable;
     };
 
     useEffect(() => {
