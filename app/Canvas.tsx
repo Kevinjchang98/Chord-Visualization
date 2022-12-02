087f2197dfde90966eaf7dec18d27e7977e7a509
Kevin Chang <kevinjchang98@gmail.com>
2022-12-01 20:45:01 -0800

    draw nodes

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -1,7 +1,43 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { select } from 'd3';
+
 export default function Canvas() {
+    const svgRef: any = useRef();
+
+    const data = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+    useEffect(() => {
+        const svg = select(svgRef.current);
+
+        let theta = (2 * Math.PI) / data.length;
+        let r = 400;
+
+        const axisCircle = svg.append('g').attr('class', 'axisCircle');
+
+        axisCircle
+            .append('circle')
+            .attr('cx', '50%')
+            .attr('cy', '50%')
+            .attr('r', r)
+            .style('stroke', 'white');
+
+        const nodeCircles = svg.append('g').attr('class', 'nodeCircles');
+
+        nodeCircles
+            .selectAll('.nodeCircles')
+            .data(data)
+            .join('circle')
+            .attr('cx', (d, i) => r * Math.cos(i * theta + Math.PI / 2) + 500)
+            .attr('cy', (d, i) => r * Math.sin(i * theta + Math.PI / 2) + 500)
+            .attr('r', 25)
+            .style('fill', 'blue');
+    }, [data]);
+
     return (
         <div>
-            <svg width="500" height="500"></svg>
+            <svg width="1000" height="1000" ref={svgRef} />
         </div>
     );
 }
