3168322ac63c5a8c688a3516e68d29e917daeba4
Kevin Chang <kevinjchang98@gmail.com>
2022-12-01 21:26:56 -0800

    allow adding of nodes

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -1,18 +1,49 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { select } from 'd3';
 
-export default function Canvas() {
-    const svgRef: any = useRef();
+interface ChordNode {
+    id: number;
+    coords: {
+        x: number;
+        y: number;
+    };
+}
 
-    const data = ['a', 'b', 'c', 'd', 'e', 'f', 'f'];
+export default function Canvas() {
+    const [ticks, setTicks] = useState<Array<String>>([]);
+    const [nodesData, setNodesData] = useState<Array<ChordNode>>([]);
+    const svgRef: any = useRef();
+    const M = 5;
+    const r = 400;
+    const theta = (2 * Math.PI) / Math.pow(2, M);
+
+    useEffect(() => {
+        let ticks = [];
+        for (let i = 0; i < Math.pow(2, M); i++) {
+            ticks.push('');
+        }
+        setTicks(ticks);
+    }, []);
+
+    const addNode = () => {
+        let id = Math.floor(Math.random() * Math.pow(2, M));
+        let coords = {
+            x: r * Math.cos(id * theta - Math.PI / 2) + 500,
+            y: r * Math.sin(id * theta - Math.PI / 2) + 500,
+        };
+
+        let newNode: ChordNode = {
+            id,
+            coords,
+        };
+
+        setNodesData([...nodesData, newNode]);
+    };
 
     useEffect(() => {
         const svg = select(svgRef.current);
-
-        let theta = (2 * Math.PI) / data.length;
-        let r = 400;
 
         const axisCircle = svg.append('g').attr('class', 'axisCircle');
 
@@ -23,21 +54,51 @@ export default function Canvas() {
             .attr('r', r)
             .style('stroke', 'white');
 
-        const nodeCircles = svg.append('g').attr('class', 'nodeCircles');
+        const axisTicks = svg.append('g').attr('class', 'axisTicks');
 
-        nodeCircles
-            .selectAll('.nodeCircles')
-            .data(data)
+        axisTicks
+            .selectAll('.axisTicks')
+            .data(ticks)
+            .join('line')
+            .attr(
+                'x1',
+                (d, i) => (r - 10) * Math.cos(i * theta - Math.PI / 2) + 500
+            )
+            .attr(
+                'x2',
+                (d, i) => (r + 10) * Math.cos(i * theta - Math.PI / 2) + 500
+            )
+            .attr(
+                'y1',
+                (d, i) => (r - 10) * Math.sin(i * theta - Math.PI / 2) + 500
+            )
+            .attr(
+                'y2',
+                (d, i) => (r + 10) * Math.sin(i * theta - Math.PI / 2) + 500
+            )
+            .style('stroke', (d) => (d === '' ? 'white' : 'red'));
+    }, [ticks]);
+
+    useEffect(() => {
+        const svg = select(svgRef.current);
+
+        const nodes = svg.append('g').attr('class', 'nodes');
+
+        nodes
+            .selectAll('.nodes')
+            .data(nodesData)
             .join('circle')
-            .attr('cx', (d, i) => r * Math.cos(i * theta - Math.PI / 2) + 500)
-            .attr('cy', (d, i) => r * Math.sin(i * theta - Math.PI / 2) + 500)
-            .attr('r', 25)
+            .attr('cx', (d) => d.coords.x)
+            .attr('cy', (d) => d.coords.y)
+            .attr('r', 20)
             .style('fill', 'blue');
-    }, [data]);
+    }, [nodesData]);
 
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
             <svg width="1000" height="1000" ref={svgRef} />
+
+            <button onClick={addNode}>Add node</button>
         </div>
     );
 }
